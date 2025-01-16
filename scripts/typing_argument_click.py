3393a2fa2d6b84ade418306acb8424923e725511
Gashkov Saveliy <120210549+Nemo121007@users.noreply.github.com>
2025-01-16 12:40:53 +0300

    [#3] fix error

--- scripts/typing_argument_click.py
+++ scripts/typing_argument_click.py
@@ -1,4 +1,5 @@
 import logging
+from pathlib import Path
 
 import click
 
@@ -40,18 +41,27 @@ def comma_separated_list(ctx, param, value):  # noqa: ARG001
     help="Файл конфигурации для чтения",
 )
 @click.option(
+    "--path",
+    type=click.STRING,
+    help="Путь к файлу или директории",
+)
+@click.option(
     "--coordinates",
     type=(float, float),
     multiple=True,
     help="Множественные значения координат (например, --coordinates 1.0 2.0 --coordinates 3.0 4.0)",
 )
-def main(name, age, pi, tags, optional, color, config, coordinates):  # noqa: PLR0913
-    """
-    Пример демонстрирует:
-    - Использование встроенных типов (str, int, float)
-    - Применение пользовательской функции для обработки значений
-    - Использование click.Choice, click.File и multi-value options
-    """
+def main(  # noqa: PLR0913
+    name: str,
+    age: int,
+    pi: float,
+    tags: list,
+    optional: str,
+    color: str,
+    config: click.File,
+    path: str,
+    coordinates: list[tuple[float, float]],
+):
     logger.info(f"Имя: {name} (тип: {type(name)})")
     logger.info(f"Возраст: {age} (тип: {type(age)})")
     logger.info(f"Число π: {pi} (тип: {type(pi)})")
@@ -64,6 +74,14 @@ def main(name, age, pi, tags, optional, color, config, coordinates):  # noqa: PL
         logger.info(f"Цвет: {color} (тип: {type(color)})")
     if config:
         logger.info(f"Содержимое файла конфигурации: {config.read()}")
+    if path:
+        # Преобразование строки в путь
+        path = Path(path)
+        # Использование пути
+        if path.exists():
+            logger.info(f"Путь существует: {path} (тип: {type(path)})")
+        else:
+            logger.warning(f"Путь не существует: {path} (тип: {type(path)})")
     if coordinates:
         logger.info(f"Координаты: {coordinates} (тип: {type(coordinates)})")
 
@@ -74,11 +92,11 @@ if __name__ == "__main__":
 # Примеры вызова из командной строки:
 # python typing_argument_click.py John 25
 # python typing_argument_click.py John 25 --color red
-# python typing_argument_click.py John 25 --config config.txt
+# python typing_argument_click.py John 25 --config ../configs/config.txt
+# python typing_argument_click.py John 25 --path ../configs/
 # python typing_argument_click.py John 25 --coordinates 1.0 2.0 --coordinates 3.0 4.0
-
 # Нюансы:
 # Click предоставляет собственные классы типов для расширенных сценариев: click.Choice, click.File,
-#   click.Path, click.IntRange,
+#   click.Path, click.IntRange
 # Click позволяет создавать пользовательские функции обработки, которые выступают в роли типов
 # Click поддерживает типизацию на уровне аргументов групп команд
