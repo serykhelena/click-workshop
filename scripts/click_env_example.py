01ec7fabccf8fd2e2e7990a0f975a38e418bfe8d
Gashkov Saveliy <120210549+Nemo121007@users.noreply.github.com>
2025-01-16 16:40:40 +0300

    [#11] fix

--- scripts/click_env_example.py
+++ scripts/click_env_example.py
@@ -8,6 +8,18 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 
+@click.command(help="Пример использования переменной среды в опции")
+@click.option("--username", envvar="USERNAME", help="Имя, задано через переменную среды USERNAME")
+def greet(username):
+    """
+    Приветствие пользователя c использованием имени из переменной среды.
+    """
+    if username:
+        click.echo(f"Привет, {username}!")
+    else:
+        click.echo("Привет, незнакомец!")
+
+
 @click.group()
 @click.pass_context
 def cli(ctx):
@@ -54,15 +66,19 @@ def add_env(ctx, key, value):
 
 
 if __name__ == "__main__":
+    cli.add_command(greet)
     cli()
 
 
 # Примеры вызова из командной строки:
 # python click_env_example.py show-env
 # python click_env_example.py add-env NEW_VAR test_value
+#       $env:USERNAME="John"
+#       python click_env_example.py greet
 
 # Нюансы:
 # ctx.ensure_object(dict): Инициализация объекта контекста, который используется для хранения
 #   состояния между командами
+# Можно сохранять и загружать данные через локальный env
 # Работа c os.environ: Переменные можно загружать из текущей среды и добавлять их в
 #   процессе выполнения
