813a0dc67767f990685dec37efc0a55a31482561
Gashkov Saveliy <120210549+Nemo121007@users.noreply.github.com>
2025-01-17 00:09:17 +0300

    [#7] flag

---
+++ scripts/flag_argparce.py
@@ -0,0 +1,36 @@
+import argparse
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Пример работы c флагами")
+
+    # Флаг, который активирует определенную опцию
+    parser.add_argument(
+        "--flag",
+        action="store_true",  # Флаг, который устанавливает значение True
+        help="Включить флаг",
+    )
+
+    # Парсим аргументы
+    args = parser.parse_args()
+
+    # Обработка флагов и вывод в зависимости от флагов
+    if args.flag:
+        logger.info("Включен флаг")
+    else:
+        logger.info("Флаг не включен")
+
+
+if __name__ == "__main__":
+    main()
+
+# Примеры вызова из командной строки:
+# python flag_argparce.py --flag
+# python flag_argparce.py
+
+# Нюансы:
+# Для определения флага используется аргумент action="store_true"
